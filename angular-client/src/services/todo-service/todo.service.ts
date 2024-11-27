66840c50fedacc3b02e1e5ac2b2f0ebc0a01b73a
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-11-27 10:33:09 +0100

    Add header

--- angular-client/src/services/todo-service/todo.service.ts
+++ angular-client/src/services/todo-service/todo.service.ts
@@ -8,7 +8,7 @@ import TodoAllResponse, {
 } from '../../models/response/todo-response';
 
 // Services
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 // Environment
 import { environment } from '../../environments/environment';
@@ -19,17 +19,31 @@ import { environment } from '../../environments/environment';
 export class TodoService {
   http = inject(HttpClient);
 
-  requestAllTodos(): Observable<TodoAllResponse> {
-    return this.http.get<TodoAllResponse>(`${environment.api}/todos`);
-  }
+  private jsonHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
 
-  requestCreateTodo(title: string): Observable<TodoResponse> {
-    return this.http.post<TodoResponse>(`${environment.api}/todos/create`, {
-      title,
+  requestAllTodos(): Observable<TodoAllResponse> {
+    return this.http.get<TodoAllResponse>(`${environment.api}/todos`, {
+      headers: this.jsonHeaders,
     });
   }
 
+  requestCreateTodo(title: string): Observable<TodoResponse> {
+    return this.http.post<TodoResponse>(
+      `${environment.api}/todos/create`,
+      {
+        title,
+      },
+      {
+        headers: this.jsonHeaders,
+      }
+    );
+  }
+
   requestDeleteTodo(id: string): Observable<TodoResponse> {
-    return this.http.delete<TodoResponse>(`${environment.api}/todos/${id}`);
+    return this.http.delete<TodoResponse>(`${environment.api}/todos/${id}`, {
+      headers: this.jsonHeaders,
+    });
   }
 }
